58a1303fae1c441deaf3c1d3c12b226f5cf69024
unknown <leonardoazeredosjc@gmail.com>
2023-04-11 16:45:36 -0300

    add funcao de criar schema e tabela caso nao exista

--- index.js
+++ index.js
@@ -9,17 +9,35 @@ const connection = mysql.createConnection({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_SCHEMA,
+});
+
+// Verifica se o banco de dados existe, se não existir, cria ele
+connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_SCHEMA}`, function (err) {
+  if (err) throw err;
+  console.log(`Banco de dados criado ou já existente!`);
+
+  // Conecta no banco de dados e cria a tabela se ela não existir
+  connection.query(`USE ${process.env.DB_SCHEMA}`, function (err) {
+    if (err) throw err;
+    connection.query(`CREATE TABLE IF NOT EXISTS ${process.env.DB_TABLE} (
+        id INT NOT NULL AUTO_INCREMENT,
+        ${process.env.COLUNA1} VARCHAR(255) NOT NULL,
+        ${process.env.COLUNA2} VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8;`, function (err) {
+        if (err) throw err;
+        console.log(`Tabela criada ou já existente!`);
+      });
+
+    console.log(`Conectado ao servidor de banco de dados!`);
+  });
 });
 
 app.use(express.json());
 
-const coluna1 = process.env.COLUNA1;
-const coluna2 = process.env.COLUNA2;
-
 app.post('/inserir', (req, res) => {
-  const sql = `INSERT INTO ${process.env.DB_SCHEMA}.${process.env.DB_TABLE} (${coluna1}, ${coluna2}) VALUES (?, ?)`;
-  connection.query(sql, [req.body[coluna1], req.body[coluna2]], (err, result) => {
+  const sql = `INSERT INTO ${process.env.DB_SCHEMA}.${process.env.DB_TABLE} (${process.env.COLUNA1}, ${process.env.COLUNA2}) VALUES (?, ?)`;
+  connection.query(sql, [req.body[process.env.COLUNA1], req.body[process.env.COLUNA2]], (err, result) => {
     if (err) {
       console.error(err);
       return res.status(500).send('Erro ao inserir dados na tabela');
